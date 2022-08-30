2cf17392f2dfb98c5a5a2eecfe6bdb71200a4c66
Guy Marshall <guymarshall9@gmail.com>
2022-08-30 22:04:07 +0100

    Fix insulin calculator page

--- scripts/insulinCalculator.js
+++ scripts/insulinCalculator.js
@@ -1,11 +1,8 @@
-function calculate_required_insulin() {
-  const carbsPer100 = $('#carbsPer100').val();
-  const gramsOnScale = $('gramsOnScale').val();
-  const insulinRatio = $('insulinRatio').val();
-
+function calculate_required_insulin(carbsPer100, gramsOnScale, insulinRatio) {
   const carbsPerGram = carbsPer100 / 100;
   const totalGramsCarbs = carbsPerGram * gramsOnScale;
   const insulinNeeded = (totalGramsCarbs / 10) * insulinRatio;
+  
   return {
     "totalCarbs": totalGramsCarbs,
     "insulinRequired": insulinNeeded
@@ -14,9 +11,12 @@ function calculate_required_insulin() {
 
 $(document).ready(() => {
   $('#calculateInsulin').on('click', () => {
-    const result = calculate_required_insulin();
+    const carbsPer100 = $('#carbsPer100').val();
+    const gramsOnScale = $('#gramsOnScale').val();
+    const insulinRatio = $('#insulinRatio').val();
+    const result = calculate_required_insulin(carbsPer100, gramsOnScale, insulinRatio);
     
-    $('#carbsResult').text(`Total carbs: ${result.totalCarbs}.`);
-    $('#insulinResult').text(`Insulin required: ${result.insulinRequired}.`);
+    $('#carbsResult').text(`Total carbs: ${result['totalCarbs']}.`);
+    $('#insulinResult').text(`Insulin required: ${result['insulinRequired']}.`);
   });
 });
