3c2ee21825853989596c5624d4e1c0e73db39c29
Guy Marshall <guymarshall9@gmail.com>
2022-08-30 21:26:18 +0100

    Remove most of PHP file

--- insulincalculator.php
+++ insulincalculator.php
@@ -1,35 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Insulin Calculator</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <label for="carbsPer100">Carbohydrate per 100g:</label>
-        <br>
-        <input type="number" id="carbsPer100" name="carbsPer100" step="0.1" autocomplete="nope" min="0">
-        <br>
-        <label for="gramsOnScale">Grams on scale</label>
-        <br>
-        <input type="number" id="gramsOnScale" name="gramsOnScale" step="0.1" autocomplete="nope" min="0">
-        <br>
-        <label for="insulinRatio">Insulin ratio (insulin per 10g carbohydrate)</label>
-        <br>
-        <input type="number" id="insulinRatio" name="insulinRatio" step="0.1" autocomplete="nope" min="0">
-        <br>
-        <br>
-        <button type="submit">Calculate</button>
-    </form>
-    <?php if (isset($_POST['carbsPer100']) && isset($_POST['gramsOnScale']) && isset($_POST['insulinRatio'])): ?>
-        <hr>
-        <p>Required insulin: <?php calculate_required_insulin($carbs_per_100, $grams_on_scale, $insulin_ratio); ?></p>
-        <hr>
-    <?php endif; ?>
-</body>
-</html>
+<p>Required insulin: <?php calculate_required_insulin($carbs_per_100, $grams_on_scale, $insulin_ratio); ?></p>
 
 <?php
 
